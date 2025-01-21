6f26746804593be217a001f5c65b42255dd284bf
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-01-21 10:50:27 -0300

    creating typing for database tables

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -21,4 +21,9 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
     return reply.status(201).send();
   });
+  app.get('/', async (request, reply) => {
+    const transactions = await knex('transactions').select('*');
+
+    return reply.send(JSON.stringify(transactions));
+  });
 }
