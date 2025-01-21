73ff6edc8523b0f2672a798f75ded96c6492c8d3
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-01-21 16:15:19 -0300

    creation of first tests

---
+++ test/transactions.spec.ts
@@ -0,0 +1,45 @@
+import { it, beforeAll, afterAll, describe, expect } from 'vitest';
+import request from 'supertest';
+import { app } from '../src/app';
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 500,
+        type: 'credit',
+      })
+      .expect(201);
+  });
+
+  it('should be able to list all transactions', async () => {
+    const transactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 500,
+        type: 'credit',
+      });
+
+    const cookies = transactionResponse.get('Set-Cookie');
+
+    const listTransactions = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200);
+
+    expect(listTransactions.body.transactions).toEqual([
+      expect.objectContaining({ title: 'New transaction', amount: 500 }),
+    ]);
+  });
+});
